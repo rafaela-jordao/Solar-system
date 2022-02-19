dd0b06940ac8f70d496a8207decb5ec4c9a811db
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-02-19 19:13:46 -0300

    requisito 9

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import MissionCard from './components/MissionCard';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
+        <MissionCard />
       </div>
     );
   }
